720facb6133c037b11473c9ddc24ec6487e5845a
Horvat Horatiu <horvat.horatiu97@gmail.com>
2022-11-21 16:41:03 +0200

    empty spring app

---
+++ src/main/java/com/horatiuhorvat/springLogInRegistrationProject/SpringLoginProjectApplication.java
@@ -0,0 +1,13 @@
+package com.horatiuhorvat.springLogInRegistrationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horatiuhorvat/springLogInRegistrationProject/SpringLoginProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horatiuhorvat.springLogInRegistrationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
